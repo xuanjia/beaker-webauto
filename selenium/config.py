a1bccfed1881a27cee58ae1546a1040670d24a5e
Xuan Jia <jason.jiaxuan@gmail.com>
2014-04-16 00:25:45 +0800

    Update test cases: access policy

--- selenium/config.py
+++ selenium/config.py
@@ -3,10 +3,12 @@ username_1_noadmin and user_admin are in group_name
 username_2_noadmin is not in group_name
 '''
 hub_url=""
-user_admin=""
-user_noadmin=""
-username_admin=""
-username_1_noadmin=""
-username_2_noadmin=""
-group_name=""
+user_admin="../firefox/autouser1"
+user_noadmin_1="../firefox/autouser2"
+user_noadmin_2="../firefox/autouser3"
+username_admin="xjia"
+username_1_noadmin="tfeng"
+username_2_noadmin="lsun"
+username_3_noadmin="gpei"
+group_name="web-auto"
 group_password=""
